0e3dfdbf6a4ef1803bf06467d186dbcb7cfe21a1
Daniel Contreras <danicurri7603@gmail.com>
2017-12-05 12:18:37 +0100

    completed models: User, Flat, Bookings)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :flats, through: :bookings
+  has_many :bookings #user as customer can have many bookings
+  has_many :flats # user as owner onws many flats
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204171925) do
+ActiveRecord::Schema.define(version: 20171205104646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer  "total_price"
+    t.date     "initial_date"
+    t.integer  "number_nights"
+    t.integer  "user_id"
+    t.integer  "flat_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.string   "address"
+    t.string   "description"
+    t.string   "title"
+    t.integer  "price"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_flats_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +51,12 @@ ActiveRecord::Schema.define(version: 20171204171925) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "flats", "users"
 end
