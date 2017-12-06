dc3d97aefeffa1bcf0b90e992a3742d4013cc244
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-06 12:04:40 +0100

    Add Gemfile random-location

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'random-location'
 
 Flat.destroy_all
 User.destroy_all
