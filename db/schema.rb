bde402867f796e71c9c78cb01fe84cd1a7bd6663
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-07 15:26:04 +0100

    Change homepage photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207095714) do
-
+ActiveRecord::Schema.define(version: 20171207104130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +36,9 @@ ActiveRecord::Schema.define(version: 20171207095714) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "photo"
     t.index ["user_id"], name: "index_flats_on_user_id", using: :btree
   end
 
