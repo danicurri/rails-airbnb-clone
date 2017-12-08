7f50a4d24ca4cefdf60a8e9ed17af8001aef9c59
Damien Dillon <damien.dillon+github@gmail.com>
2017-12-08 16:52:41 +0100

    Add stciky map on index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208140227) do
+ActiveRecord::Schema.define(version: 20171208135556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,23 +29,6 @@ ActiveRecord::Schema.define(version: 20171208140227) do
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
-  create_table "dashboards", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_dashboards_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_dashboards_on_reset_password_token", unique: true, using: :btree
-  end
-
   create_table "flats", force: :cascade do |t|
     t.string   "address"
     t.string   "description"
@@ -54,9 +37,9 @@ ActiveRecord::Schema.define(version: 20171208140227) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "photo"
     t.index ["user_id"], name: "index_flats_on_user_id", using: :btree
   end
 
