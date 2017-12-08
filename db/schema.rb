381b80d42c5072ae8ff290a8d137d7fc6f43f5dc
John Hawley <jehawley@gmail.com>
2017-12-08 12:06:05 +0100

    added flats to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171207143401) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +36,9 @@ ActiveRecord::Schema.define(version: 20171207143401) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "photo"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "photo"
     t.index ["user_id"], name: "index_flats_on_user_id", using: :btree
   end
 
